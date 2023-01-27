e31dbff177be71979191446506388217497f97f1
Mohanraj1100 <mohanraj.anbuselvam@tringapps.com>
2023-01-27 17:49:30 +0530

    initial

---
+++ src/main/java/assignment/credit/Credit.java
@@ -0,0 +1,23 @@
+package assignment.credit;
+import java.util.*;
+public class Credit 
+{
+	public static void main(String[] args) {
+        Scanner si = new Scanner(System.in);
+
+        System.out.println("Enter cardholder name");
+        String cardholder = si.nextLine();
+        System.out.println("Enter cardnumber");
+        Long cardnumber = si.nextLong();
+        System.out.println("Enter card's expiry daate");
+        String expirydate = si.next();
+
+        Long validcardno = 987654321l;
+
+        Creditcard obj1 = new Creditcard(cardholder, cardnumber, expirydate);
+        Creditcard obj2 = obj1.co();
+        obj2.display();
+        System.out.println(obj2.equals(validcardno));
+    }
+
+}
---
+++ src/main/java/assignment/credit/Creditcard.java
@@ -0,0 +1,34 @@
+package assignment.credit;
+
+public class Creditcard implements Cloneable{
+    String cardholder;
+    Long cardnumber;
+    String expirydate;
+
+    Creditcard(String cardholder, Long cardnumber, String expirydate) {
+        this.cardholder = cardholder;
+        this.cardnumber = cardnumber;
+        this.expirydate = expirydate;
+    }
+
+    boolean equals(Long validcardno) {
+        return cardnumber.equals(validcardno);
+    }
+
+    void display() {
+        System.out.println("Cardholder nsme id" + cardholder + "and card Number id " + cardnumber + "And expiry date"
+                + expirydate);
+    }
+
+    Creditcard co() {
+    	try{
+    		return (Creditcard)super.clone();
+    		
+    	}
+    	catch(CloneNotSupportedException e)
+    	{
+    		System.out.println("Clowning error");
+    		return this;
+    	}
+}
+}
